66d113c015449240ee6624ea2b79e54c712ad60e
Otávio Menhart <otaviomenhart@hotmail.com>
2026-03-09 07:49:31 -0300

    chore: update ef core version annotation to 10.0.3
    Updated AppDbContextModelSnapshot.cs to reflect EF Core 10.0.3 usage.

--- CaseItau.Infra/Migrations/AppDbContextModelSnapshot.cs
+++ CaseItau.Infra/Migrations/AppDbContextModelSnapshot.cs
@@ -16,7 +16,7 @@ namespace CaseItau.Infra.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "9.0.7")
+                .HasAnnotation("ProductVersion", "10.0.3")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128);
 
             SqlServerModelBuilderExtensions.UseIdentityColumns(modelBuilder);
